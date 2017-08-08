f62018d07ddfb5b3a1df21779f1d9a402bbb6b83
sunaliseth <sunaliseth01@gmail.com>
2017-08-08 16:27:57 +0100

    Session 1 challenges finished tested and commited

--- session1/challenge/1_arithmetic.rb
+++ session1/challenge/1_arithmetic.rb
@@ -3,11 +3,16 @@
 #  $ rake 1:1
 
 # Given a number, return 20 less than, that number multiplied by 5
-# 
+#
 # arithmetic1(10)     # => 30
 # arithmeitc1(10.5)   # => 32.5
 # arithmeitc1(-6)     # => -50
 
 def arithmetic1(n)
   # the code for this method goes in here
+  p = (n * 5) - 20
+  return p
 end
+arithmetic1(10)
+arithmetic1(10.5)
+arithmetic1(-6)
